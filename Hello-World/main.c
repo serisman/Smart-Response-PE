60f5840cd66705930541cc3ab3ccb12de9959272
serisman <git@serisman.com>
2018-09-15 00:29:15 -0500

    Add a simple UART module (TX only for now)

--- Hello-World/main.c
+++ Hello-World/main.c
@@ -4,10 +4,16 @@
 // Creation Date: 2018-09-14
 // License: MIT
 
+#include <stdio.h>
+
 #include "hardware.h"
 #include "clock.h"
+#include "uart.h"
 
 inline void loop() {
+	uint32_t millis = clock_millis();
+	printf("millis: ");	put_ul(millis);	printf("\r\n");
+
 	PIN_RED_LED = 1;
 	PIN_GREEN_LED = 0;
 	PIN_TX_LED = 1;
@@ -30,16 +36,21 @@ inline void loop() {
 }
 
 inline void setup() {
+	// TODO... set clock to 32 MHz on boot?
 	clock_init();
 
-	P0DIR = 0xFF;	// Port 0 - set as outputs
-	P0 = 0x00; 		// Port 0 - set all low
+	P0 = 0xC0; 			// Port 0 - set all low (except TxD/RxD)
+	P0DIR = 0x5F;		// Port 0 - set as outputs (except RxD/CTS)
+
+	uart_init();
+	clock_delay_ms(10);	// let the baud rate timer settle in?
+	printf("Hello World!\r\n");
 }
 
 void main() {
+	ENABLE_INTERRUPTS;
 	setup();
 
-	ENABLE_INTERRUPTS;
 	for(;;) {
 		loop();
 	}
